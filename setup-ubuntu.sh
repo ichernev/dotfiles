d8d3fa2a38de1a7fec2ecbd5f473c03a21d84472
Iskren Chernev <iskren.chernev@gmail.com>
2014-06-23 22:29:15 +0000

    Use ppa for git in ubuntu

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:git-core/ppa
+
 sudo apt-get install zsh git xmonad vim-gtk rxvt-unicode-256color fonts-droid \
   suckless-tools htop xsel feh curl gawk
 
