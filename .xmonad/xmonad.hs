405ae18d1383ca8b5a4c38e87d0a2aaa0f041bce
Iskren Chernev <iskren.chernev@gmail.com>
2018-11-03 21:32:07 -0700

    Enable scratchpad, add screenshot/cropscreen utils and shortcuts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,13 +8,23 @@ import XMonad.Layout.NoBorders(smartBorders)
 import XMonad.Hooks.ManageDocks(avoidStruts,ToggleStruts(..),docksStartupHook,docksEventHook)
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Layout.Fullscreen(fullscreenEventHook)
+import XMonad.Util.Scratchpad(scratchpadSpawnActionTerminal,scratchpadManageHook)
 import qualified XMonad.Hooks.PerWindowKbdLayout as PWKbdLayout
+import qualified XMonad.StackSet as W
 
 myTerminal = "urxvt"
 
 -- xFont = "-*-terminus-medium-r-normal-*-18-*-*-*-*-*-iso10646-*"
 xFont = "Droid Sans Mono-12"
 dmenuRun = "dmenu_run -fn \"" ++ xFont ++ "\" -nb '#000' -nf '#aaa' -sb '#aaa' -sf '#000'"
+
+manageScratchPad :: ManageHook
+manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
+  where
+    h = 0.8     -- terminal height, 40%
+    w = 0.9     -- terminal width,  90%
+    t = (1 - h) / 2     -- distance from top edge, 20%
+    l = (1 - w) / 2    -- distance from left edge, 5%
 
 myManageHook = composeAll
     [
@@ -40,7 +50,7 @@ main = do
           modMask = mod4Mask
         , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
         , terminal = myTerminal
-        , manageHook = myManageHook <+> manageHook defaultConfig
+        , manageHook = myManageHook <+> manageScratchPad <+> manageHook defaultConfig
         , logHook = takeTopFocus
                     <+> dynamicLogWithPP xmobarPP
                        { ppOutput = hPutStrLn xmproc
@@ -64,4 +74,10 @@ main = do
         , ("M-c", spawn "xcopy")
         , ("M-v", spawn "xselectcb")
         , ("M-S-l", spawn "slock")
+        -- scratchpad
+        , ("M-`", scratchpadSpawnActionTerminal myTerminal)
+        -- screenshot (full screen)
+        , ("M-S-s", spawn "screenshot")
+        -- screenshot (part of screen)
+        , ("M-S-p", spawn "cropscreen")
         ]
