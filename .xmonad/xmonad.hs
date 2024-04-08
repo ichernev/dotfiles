71981017d5278dfcf502fcfa28e9bc1eff668059
Iskren Chernev <me@iskren.info>
2024-04-08 23:46:43 +0300

    Wrap dmenu_run in launcher

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,8 +18,8 @@ import qualified XMonad.StackSet as W
 myTerminal = "urxvt"
 
 -- xFont = "-*-terminus-medium-r-normal-*-18-*-*-*-*-*-iso10646-*"
-xFont = "Droid Sans Mono-13"
-dmenuRun = "dmenu_run -fn \"" ++ xFont ++ "\" -nb '#000' -nf '#aaa' -sb '#aaa' -sf '#000'"
+-- xFont = "Droid Sans Mono-15"
+-- dmenuRun = "dmenu_run -fn \"" ++ xFont ++ "\" -nb '#000' -nf '#aaa' -sb '#aaa' -sf '#000'"
 
 manageScratchPad :: ManageHook
 manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
@@ -42,9 +42,10 @@ myManageHook = composeAll
     -- , className =? "Chromium-browser" --> doShift "mail"
     -- , className =? "Chromium"         --> doShift "web"
     -- , className =? "Google-chrome"    --> doShift "web"
-    , className =? "spotify"          --> doShift "spotify"
+    , className =? "Spotify"          --> doShift "spotify"
     , className =? "Cssh"             --> doFloat
     , className =? "nm-openconnect-auth-dialog" --> doShift "spotify"
+    , stringProperty "WM_WINDOW_ROLE" =? "WebRTCGlobalIndicator" --> doFloat
     ]
 
 extraWs = ["F" ++ (show i) | i <- [1..5]]
@@ -71,16 +72,16 @@ main = do
         } `additionalKeysP`
         ([
           ("M-b", sendMessage ToggleStruts)
-        , ("M-p", spawn dmenuRun)
+        , ("M-p", spawn "launcher")
         , ("M-<Left>", prevWS)
         , ("M-<Right>", nextWS)
         , ("M-C-f", spawn "brightness -")
         , ("M-C-g", spawn "brightness +")
-        , ("M-C-r", spawn "vol_ctl -")
-        , ("M-C-t", spawn "vol_ctl +")
+        , ("M-C-r", spawn "pamixer -d 3")
+        , ("M-C-t", spawn "pamixer -i 3")
         , ("M-c", spawn "xcopy")
-        , ("M-S-v", spawn "xpaste -p")
-        , ("M-v", spawn "xpaste -b")
+        --, ("M-S-v", spawn "xpaste -p")
+        --, ("M-v", spawn "xpaste -b")
         , ("M-S-l", spawn "slock")
         -- scratchpad
         , ("M-`", scratchpadSpawnActionTerminal myTerminal)
